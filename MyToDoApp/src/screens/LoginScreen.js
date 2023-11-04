6ecace8cfde83e135dee91478a72ffecf529a99c
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-04 15:14:30 +0530

    Update LoginScreen.js

--- MyToDoApp/src/screens/LoginScreen.js
+++ MyToDoApp/src/screens/LoginScreen.js
@@ -1,23 +1,42 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import LoginForm from './LoginForm';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
 
 const LoginScreen = ({ navigation }) => {
-  const handleLogin = (email, password) => {
-    // Implement your authentication logic here.
-    // You can make an API request or perform any other authentication process.
-    if (email && password) {
-      // If authentication is successful, navigate to the next screen (e.g., HomeScreen).
-      navigation.navigate('Home');
-    } else {
-      // Handle authentication failure (e.g., display an error message).
-      alert('Authentication failed. Please check your credentials.');
-    }
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isRegistering, setIsRegistering] = useState(false);
+
+  const handleLogin = () => {
+    // Implement the logic to handle login here
+    // You can send the email and password to your authentication API
+  };
+
+  const handleSwitchMode = () => {
+    setIsRegistering(!isRegistering);
   };
 
   return (
     <View style={styles.container}>
-      <LoginForm onLogin={handleLogin} />
+      <View style={styles.formContainer}>
+        <Text style={styles.title}>{isRegistering ? 'Register' : 'Login'} Page</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          value={email}
+          onChangeText={(text) => setEmail(text)}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          secureTextEntry
+          value={password}
+          onChangeText={(text) => setPassword(text)}
+        />
+        <Button title={isRegistering ? 'Register' : 'Login'} onPress={handleLogin} />
+        <Text style={styles.switchText} onPress={handleSwitchMode}>
+          {isRegistering ? 'Switch to Login' : 'Switch to Register'}
+        </Text>
+      </View>
     </View>
   );
 };
@@ -25,8 +44,36 @@ const LoginScreen = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: 'gray',
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 16,
+  },
+  formContainer: {
+    backgroundColor: 'white',
+    padding: 16,
+    borderRadius: 8,
+    width: '100%',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  input: {
+    width: '100%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 12,
+    paddingHorizontal: 12,
+  },
+  switchText: {
+    textAlign: 'center',
+    marginTop: 8,
+    color: 'blue', // You can customize the color
+    textDecorationLine: 'underline',
   },
 });
 
