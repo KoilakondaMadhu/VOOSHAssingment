e7cf952cd7b0b3cd6b0b05b2ce90d986acf7fa8c
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-04 15:24:08 +0530

    Update ProfileScreen.js

--- MyToDoApp/src/screens/ProfileScreen.js
+++ MyToDoApp/src/screens/ProfileScreen.js
@@ -1,20 +1,58 @@
-import React, { useState } from 'react';
-import UserProfile from './UserProfile';
+import React, { useState, useEffect } from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { getUserDetails, clearUserData } from './api'; // Replace with your actual API functions
 
 const ProfileScreen = ({ navigation }) => {
-  const [user] = useState({
-    name: 'John Doe',
-    email: 'johndoe@example.com',
-    // Add more user data as needed
-  });
+  const [userDetails, setUserDetails] = useState(null);
+
+  const fetchUserDetails = async () => {
+    try {
+      // Replace with your API function to fetch user details using the access token
+      const response = await getUserDetails();
+      setUserDetails(response.data); // Assuming your API returns user details in the 'data' field
+    } catch (error) {
+      console.error('Error fetching user details:', error);
+    }
+  };
 
   const handleLogout = () => {
-    // Implement your logout logic here (e.g., clear user data and navigate to the login screen).
-    // You can use AsyncStorage or a similar storage method to manage user data.
+    // Implement your logout logic here, including clearing user data and navigating to the login screen
+    clearUserData(); // Replace with the function to clear user data
     navigation.navigate('Login');
   };
 
-  return <UserProfile user={user} onLogout={handleLogout} />;
+  useEffect(() => {
+    fetchUserDetails();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Profile Page</Text>
+      {userDetails ? (
+        <View>
+          <Text>Name: {userDetails.name}</Text>
+          <Text>Email: {userDetails.email}</Text>
+          {/* Display other user details as needed */}
+        </View>
+      ) : (
+        <Text>Loading user details...</Text>
+      )}
+      <Button title="Logout" onPress={handleLogout} />
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+});
 
 export default ProfileScreen;
